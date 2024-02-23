d962f6bb3020c9850e3599a31ef7f975cc998361
wannarollwithagit <110957732+wannarollwithagit@users.noreply.github.com>
2024-02-23 14:02:08 +1100

    Create basic calculation functions

---
+++ main.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return "OPERATOR ERROR";
+    }
+}
+
+let num1 = 2;
+let num2 = 3;
+let operator = '/';
+console.log(operate(operator, num1, num2));
